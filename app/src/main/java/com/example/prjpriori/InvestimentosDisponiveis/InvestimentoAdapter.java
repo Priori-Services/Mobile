589925572140614c692c2e0ecc5d46c565fa9872
gabriel <gaamdal557@gmail.com>
2023-05-03 23:40:17 -0300

    fix: arrumando layout

--- app/src/main/java/com/example/prjpriori/InvestimentosDisponiveis/InvestimentoAdapter.java
+++ app/src/main/java/com/example/prjpriori/InvestimentosDisponiveis/InvestimentoAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.prjpriori.R;
@@ -12,6 +13,7 @@ import com.example.prjpriori.R;
 import java.util.ArrayList;
 
 public class InvestimentoAdapter extends BaseAdapter {
+
 
     private Context contexto;
 
